70c507c2d09b05ef030db1cbfd32508b8a287527
Student 6 <Student 6@10.8.33.106>
2017-05-04 12:54:28 -0400

    stuff

--- src/main/Enemy.java
+++ src/main/Enemy.java
@@ -1,11 +1,15 @@
 package main;
 
 import gui.components.Graphic;
+import gui.components.MovingComponent;
 
-public class Enemy extends Graphic{
-
-	public Enemy(int x, int y, String photo) {
-		super(x,y,photo);
+public class Enemy extends MovingComponent{
+	private int z;
+	private String photoLocation;
+	public Enemy(int x, int y, int w, int h,int z,String photo) {
+		super(x,y,w,h);
+		this.photoLocation = photo;
+		this.z = z;
 	}
 
 }
--- src/main/ScreenGame.java
+++ src/main/ScreenGame.java
@@ -1,16 +1,22 @@
 package main;
 
+import java.awt.event.KeyEvent;
+
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import gui.Screen;
 import gui.components.Graphic;
 import gui.components.Visible;
 
-public class ScreenGame extends Screen {
-
+public class ScreenGame extends Screen implements KeyListener, Runnable{
+	private List<Enemy> stuff;
+	private boolean gameRunning;
 	public ScreenGame(int width, int height) {
 		super(width, height);
-		
+		gameRunning = true;
 	}
 
 	@Override
@@ -18,6 +24,35 @@ public class ScreenGame extends Screen {
 		Graphic x = new Graphic(100,100, "resources/square.png");
 		viewObjects.add(x);
 
+	}
+
+	@Override
+	public KeyListener getKeyListener(){
+		return this;
+	}
+	public void keyPressed(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		stuff = Collections.synchronizedList(new ArrayList<Enemy>());
+		while(gameRunning){
+			
+		}
 	}
 
 }
