ee148917661db69567832f18cd9223ddfdb030d3
Student 6 <Student 6@10.8.33.106>
2017-05-17 12:30:15 -0400

    dwadwa

--- src/main/ScreenGame.java
+++ src/main/ScreenGame.java
@@ -70,6 +70,7 @@ public class ScreenGame extends Screen implements KeyListener, MouseListener,Run
 		viewObjects.add(p2);
 		p2.play();
 		
+		
 //		Enemy e = new Enemy(450,350,50,50, 450, "resources/triangle.png");
 //		e.setAction(new Action(){
 //			public void act(){
